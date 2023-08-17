8c0789ee7e1fb651582b8f15b2182b4b36ea988a
Jack Thomas <jack.h.thomas567@gmail.com>
2023-08-17 15:16:18 -0700

    added model and service unit tests

---
+++ link/tests/test_models.py
@@ -0,0 +1,9 @@
+from models.models import Link
+
+
+def test_link_model():
+    link = Link("abc12345", "https://www.example.com", "https://pa.ni/abc12345")
+
+    assert link.id == "abc12345"
+    assert link.long_url == "https://www.example.com"
+    assert link.short_url == "https://pa.ni/abc12345"
---
+++ link/tests/test_services.py
@@ -0,0 +1,12 @@
+from services.services import LinkService
+
+
+def test_generate_short_url():
+    link_service = LinkService()
+    long_url = "https://www.example.com/some/long/url"
+
+    link = link_service.generate_shorten_url(long_url)
+
+    assert link.id
+    assert link.long_url == long_url
+    assert link.short_url.startswith("https://pa.ni/")
