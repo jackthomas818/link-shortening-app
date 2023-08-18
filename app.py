e4511f3493005c28c9656fdad5c3071e440704a8
Jack Thomas <jack.h.thomas567@gmail.com>
2023-08-17 17:50:58 -0700

    added redirect to /ui/ from root

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask
+from flask import Flask, redirect, url_for
 from flask_cors import CORS
+
 from link.routes.routes import link_blueprint
 from ui.routes.routes import ui_blueprint
 
@@ -9,6 +10,13 @@ app.register_blueprint(ui_blueprint, url_prefix="/ui")
 
 # Configure allowed origins in production environment.
 CORS(app)
+
+
+# Redirect root URL to /ui
+@app.route("/")
+def root():
+    return redirect(url_for("ui.index"))
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
